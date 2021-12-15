4eeca2e529644f92913e34b82d211aba10beda89
laurilako <71126961+laurilako@users.noreply.github.com>
2021-12-15 21:38:24 +0200

    trying to fix background lag

--- src/components/ParticleBg.js
+++ src/components/ParticleBg.js
@@ -10,7 +10,7 @@ function ParticleBg() {
   };
   
   const particlesLoaded = (container) => {
-    console.log(container);
+    // console.log(container);
   };
   
   const config = useColorModeValue(particlesConfigLight, particlesConfigDark)
